145d08b55361face4c0c7ca367b492792025e559
xion <x@xs-MacBook-Pro.local>
2011-02-03 15:23:49 +0700

    removed base64 linebreaks

--- app/helpers/mobile_helper.rb
+++ app/helpers/mobile_helper.rb
@@ -2,7 +2,7 @@ module MobileHelper
   def image_to_base64(path)
     file = File.open(PUBLIC_FOLDER + path, "r")
     mime_type = "image/jpeg"
-    base64_content = ActiveSupport::Base64.encode64(file.read)
+    base64_content = ActiveSupport::Base64.encode64(file.read).gsub("\n", "")
     "data:#{mime_type};base64,#{base64_content}"
   end
   
